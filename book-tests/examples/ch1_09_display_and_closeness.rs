f6ab2a374356a4520341a6a1719fd844f6b1e0e9
jhosein58 <jhosein58@gmail.com>
2026-07-10 16:39:36 +0330

    updated: ch3_03_structured_and_batched

--- book-tests/examples/ch1_09_display_and_closeness.rs
+++ book-tests/examples/ch1_09_display_and_closeness.rs
@@ -4,16 +4,16 @@ use burn::tensor::{Tensor, check_closeness};
 type Backend = NdArray;
 
 fn main() {
-    let device: <Backend as burn::tensor::backend::Backend>::Device = Default::default();
+    let dev = Default::default();
 
-    let tensor = Tensor::<Backend, 2>::full([2, 3], 0.123456789, &device);
+    let tensor = Tensor::<Backend, 2>::full([2, 3], 0.123456789, &dev);
     println!("{}", tensor);
 
     // control precision with standard Rust formatting
     println!("{:.2}", tensor);
 
-    let a = Tensor::<Backend, 1>::from_floats([1.0, 2.0, 3.0, 4.0, 5.0], &device);
-    let b = Tensor::<Backend, 1>::from_floats([1.0, 2.0, 3.0, 4.0, 5.001], &device);
+    let a = Tensor::<Backend, 1>::from_floats([1.0, 2.0, 3.0, 4.0, 5.0], &dev);
+    let b = Tensor::<Backend, 1>::from_floats([1.0, 2.0, 3.0, 4.0, 5.001], &dev);
 
     check_closeness(&a, &b);
 }
